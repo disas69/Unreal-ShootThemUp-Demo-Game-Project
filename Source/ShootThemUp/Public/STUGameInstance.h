e6c09f0b35a69b87e37cea3dbb832d9e85f9c018
Eugene <evgenii.esaulenko@gmail.com>
2024-04-14 17:15:24 +0200

    Add custom GameInstance class

---
+++ Source/ShootThemUp/Public/STUGameInstance.h
@@ -0,0 +1,13 @@
+// Shoot Them Up demo game project. Evgenii Esaulenko, 2024
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "STUGameInstance.generated.h"
+
+UCLASS()
+class SHOOTTHEMUP_API USTUGameInstance : public UGameInstance
+{
+    GENERATED_BODY()
+};
