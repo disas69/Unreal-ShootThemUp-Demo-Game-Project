1c70e8747c2be21ce04b4a59ecde6c2c602dc28a
Eugene <evgenii.esaulenko@gmail.com>
2024-07-21 20:01:57 +0200

    Update project to v5.4

--- Source/ShootThemUp.Target.cs
+++ Source/ShootThemUp.Target.cs
@@ -7,7 +7,8 @@ public class ShootThemUpTarget : TargetRules
 	public ShootThemUpTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
 
 		ExtraModuleNames.AddRange(new[] { "ShootThemUp" });
 	}
--- Source/ShootThemUpEditor.Target.cs
+++ Source/ShootThemUpEditor.Target.cs
@@ -7,7 +7,8 @@ public class ShootThemUpEditorTarget : TargetRules
 	public ShootThemUpEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
 
 		ExtraModuleNames.AddRange(new[] { "ShootThemUp" });
 	}
